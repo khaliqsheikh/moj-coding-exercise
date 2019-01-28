65a79292cde9589725dcce91af516086c93169b0
Mohammad Khaliq Sheikh <m.khaliq.sheikh@gmail.com>
2019-01-28 12:59:14 +0000

    Initial Changes

---
+++ moj-coding-exercise/src/main/java/com/coding/example/MojCodingExerciseApplication.java
@@ -0,0 +1,14 @@
+package com.coding.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojCodingExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojCodingExerciseApplication.class, args);
+	}
+
+}
+
